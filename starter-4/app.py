f2963401cc28514f7797fba68560d6bb114a2363
gabyfuki <gabyfuki@gmail.com>
2026-04-09 14:40:53 -0400

    Update app.py

--- starter-4/app.py
+++ starter-4/app.py
@@ -1,5 +1,6 @@
 import time
 
+import os
 import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request
@@ -7,7 +8,31 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 datasets = ["blobs", "circles", "lines", "moons", "uniform"]
 
+# get the points as a numpy array 
+def load_dataset(name):
+    path = os.path.join("static","datasets", f"{name}.csv")
+    df = pd.read_csv(path)
+    return df.to_numpy()
 
+def assign_clusters(points, centroids):
+    # calculate distances of each point from eahc centroid
+    distances = np.linalg.norm(points[:, np.newaxis] - centroids, axis=2)
+    # return the centroid that minimizes distance for each point
+    return np.argmin(distances, axis=1)
+
+def update_centroids(points, old_centroids, n, assignments):
+    new_centroids = []
+    #for i in range(n):
+
+
+
+
+# what we will need to pass to front end:
+# points
+# centroids
+# assignment
+# iteration 
+# dataset
 @app.route("/")
 def index():
     return render_template("index.html", datasets=datasets)
